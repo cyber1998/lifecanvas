db9850cb6054dadd5191500aa31215af5b64d55f
Cyber <cybr1998@gmail.com>
2022-03-31 07:46:32 +0530

    Add views for Journal and Chapter

--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
 # Create your views here.
+from journal.models import Journal, Chapter
+from journal.serializers import JournalSerializer, ChapterSerializer
+from django.db.models import Q
+
+class JournalModelViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and creating Journal entries.
+    """
+    model = Journal
+    serializer_class = JournalSerializer
+
+    def get_queryset(self):
+        queryset = Journal.objects.filter(
+            Q(created_by=self.request.user) | Q(is_public=True)
+        )
+        return queryset
+
+
+class ChapterModelViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and creating Journal entries.
+    """
+    model = Chapter
+    serializer_class = ChapterSerializer
