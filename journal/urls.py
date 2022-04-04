c758fcece4e6707dbf721bab3bf43b59193a514f
Cyber <cybr1998@gmail.com>
2022-04-04 08:41:22 +0530

    urls for chapter views

--- journal/urls.py
+++ journal/urls.py
@@ -3,6 +3,9 @@ from journal import views
 router = routers.SimpleRouter(trailing_slash=True)
 
 router.register('', views.JournalModelViewSet, basename='journal')
-router.register(r'^(?P<journal_id>[0-9]+)/chapter', views.ChapterModelViewSet, basename='chapter')
+router.register(r'^(?P<journal_id>[0-9]+)/chapter',
+                views.ChapterModelViewSet, basename='chapter')
+router.register(r'^(?P<journal_id>[0-9]+)/chapter/(?P<chapter_id>[0-9]+)/view',
+                views.ChapterViewsViewSet, basename='chapter_views')
 
 urlpatterns = router.urls
