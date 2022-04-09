76d5936809df0a3c8f942c644c7abf8509c38f41
Cyber <cybr1998@gmail.com>
2022-04-09 14:11:54 +0530

    Fix signals

--- common/signals.py
+++ common/signals.py
@@ -6,8 +6,15 @@ from userprofile.models import UserProfile
 
 
 @receiver(post_save, sender=User, dispatch_uid="create_user_profile")
-def create_user_profile(sender, instance):
+def create_user_profile(sender, instance, *args, **kwargs):
     """
     Create a UserProfile object when a new User object is created.
     """
     UserProfile.objects.create(user=instance)
+
+
+post_save.connect(
+    create_user_profile,
+    sender=User,
+    dispatch_uid='create_user_profile'
+)
