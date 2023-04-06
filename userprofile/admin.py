3bb27a725bb7cee6b138021ef218c8bc1f897faf
Cyber <cybr1998@gmail.com>
2023-04-06 08:39:09 +0530

    Supporting admin changes

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -5,3 +5,7 @@ from userprofile import models
 @admin.register(models.UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'is_active', 'is_private', 'created_at')
+
+@admin.register(models.Interest)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'is_active', 'name')
