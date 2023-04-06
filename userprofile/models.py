5e7c4b103c4fcd66d896631c07d7567f93895618
Cyber <cybr1998@gmail.com>
2023-04-06 08:39:00 +0530

    Add interest and user profile interest

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,8 +1,20 @@
 from django.contrib.auth.models import User
 from django.db import models
 from common.abstracts import (
-    AbstractTimestamp
+    AbstractTimestamp,
+    AbstractModel
 )
+
+class Interest(AbstractModel):
+    name = models.CharField(max_length=128)
+
+    class Meta:
+        db_table = 'interest'
+        verbose_name = 'Interest'
+        verbose_name_plural = 'Interests'
+
+    def __str__(self):
+        return self.name
 
 
 class UserProfile(AbstractTimestamp):
@@ -15,6 +27,7 @@ class UserProfile(AbstractTimestamp):
         related_name='%(app_label)s_%(class)s_associated_userprofile'
     )
     is_private = models.BooleanField(default=False)
+    interests = models.ManyToManyField(Interest, related_name='interests')
 
     class Meta:
         db_table = 'user_profile'
@@ -22,4 +35,4 @@ class UserProfile(AbstractTimestamp):
         verbose_name_plural = 'User Profiles'
 
     def __str__(self):
-        return self.user
+        return self.user.first_name
