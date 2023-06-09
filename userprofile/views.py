684e0858ff792093c110928ea21b86aff2d679e6
Cyber <cybr1998@gmail.com>
2023-06-09 09:11:26 +0530

    Refactor the url structure

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from userprofile.models import UserProfile
 from django.contrib.auth.models import User
 from userprofile.serializers import UserProfileSerializer, UserSerializer
 
 # Create your views here.
+
+
 class UserViewSet(viewsets.ModelViewSet):
     """
     A viewset for viewing and creating Users.
     """
     model = User
     serializer_class = UserSerializer
-    permission_classes = []
     queryset = User.objects.all()
-
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """
@@ -22,8 +24,15 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     model = UserProfile
     serializer_class = UserProfileSerializer
 
-    def get_queryset(self):
-        return UserProfile.objects.filter(
-            is_active=True,
-            is_private=False,
-        )
+
+@api_view(['GET'])
+def get_user_profile(request, user_id=None):
+    """
+    Get a User Profile entry by user id
+    """
+    try:
+        user_profile = UserProfile.objects.get(user_id=user_id)
+        serializer = UserProfileSerializer(user_profile)
+        return Response(serializer.data)
+    except UserProfile.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
