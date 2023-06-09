850489fb7b7fb31100ea256e2a29389d714ba053
Cyber <cybr1998@gmail.com>
2023-06-09 08:06:35 +0530

    remove unused import

--- canvas_fe/src/App.js
+++ canvas_fe/src/App.js
@@ -1,8 +1,6 @@
 import React, {Component, Fragment} from "react";
 import Header from "./components/header";
 import Home from "./components/home";
-import UserList from "./components/users/list";
-
 
 
 class App extends Component {
