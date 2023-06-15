6469a7a1bf33a53621c403701a1c279d5a77f025
Cyber <cybr1998@gmail.com>
2023-06-15 21:25:21 +0530

    Custom token serializer to return name

--- common/views.py
+++ common/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from common.serializers import CanvasTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    # Replace the serializer with your custom
+    serializer_class = CanvasTokenObtainPairSerializer
