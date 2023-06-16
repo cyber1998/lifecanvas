298443dcd095ee002c10f2deb52bbe034c4ac14b
Cyber <cybr1998@gmail.com>
2023-06-16 08:41:50 +0530

    Seperate out chapters a bit

--- canvas_fe/src/components/chapters/ChapterList.jsx
+++ canvas_fe/src/components/chapters/ChapterList.jsx
@@ -7,8 +7,8 @@ const ChapterList = ({ chapters }) => {
   return (
     <>
       {chapters.map((chapter) => (
-        <Col key={chapter.id}>
-          <Card className="mb-3">
+        <Col key={chapter.id} xs="12" sm="6" md="6" lg="6" xl="6">
+          <Card  className="mb-3">
             <div className="card-body">
               <h5 className="card-title">{chapter.title}</h5>
               <p className="card-text">{chapter.description}</p>
