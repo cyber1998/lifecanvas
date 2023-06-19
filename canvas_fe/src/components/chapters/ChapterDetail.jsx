cee14cd8fe383fe9c544677416debd3ef6ff3188
Cyber <cybr1998@gmail.com>
2023-06-19 09:02:11 +0530

    basic prev and next component for chapters

--- canvas_fe/src/components/chapters/ChapterDetail.jsx
+++ canvas_fe/src/components/chapters/ChapterDetail.jsx
@@ -1,7 +1,20 @@
-import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
+import { axiosInstance, BASE_API_URL } from '../../constants';
 
-const ChapterDetail = ({ isOpen, toggle, currentChapter }) => {
+const ChapterDetail = ({ isOpen, toggle, currentChapter, currentJournalId, setCurrentChapter}) => {
 
+    const getChapter = (chapter_id) => {
+        axiosInstance
+          .get(BASE_API_URL + `journal/${currentJournalId}/chapter/${chapter_id}/`)
+          .then((res) => setChapter(res.data));
+    };
+
+    const setChapter = (chapter) => {
+        setCurrentChapter(chapter);
+    };
+
+    console.log(currentChapter);
+    
     return (
         <>
         <Modal isOpen={isOpen} toggle={toggle} className="modal-lg">
@@ -24,6 +37,20 @@ const ChapterDetail = ({ isOpen, toggle, currentChapter }) => {
                     </span>
                 </div>
             </ModalBody>
+            <ModalFooter>
+                <div className="d-flex flex-column min-h-100 justify-content-center align-items-center mb-3">
+                    <span className="align-left">
+                        <Button color="primary" onClick={() => getChapter(currentChapter.previous_chapter)} disabled={currentChapter.previous_chapter === null}>
+                            Previous
+                        </Button>
+                    </span>
+                    <span className="align-right">
+                        <Button color="primary" onClick={() => getChapter(currentChapter.next_chapter)} disabled={currentChapter.next_chapter === null}>
+                            Next
+                        </Button>
+                    </span>
+                </div>
+            </ModalFooter>
         </Modal>
         </>
     );
