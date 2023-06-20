fefac6b65a4fa574d2a6652f6571125ca81061e6
Cyber <cybr1998@gmail.com>
2023-06-20 07:54:04 +0530

    Directly access user from props

--- canvas_fe/src/components/Home.jsx
+++ canvas_fe/src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Home = (props) => {
+const Home = ({user}) => {
   return (
     <>
     <div id="main-container">
       <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center" id="welcome-element">
         <span className="align-middle">
-          <h2>Welcome to Lifecanvas, {props.firstName}</h2>
+          <h2>Welcome to Lifecanvas, {user.first_name}</h2>
         </span>
         <span className="align-middle">
           <p>Start writing your journal here!</p>
