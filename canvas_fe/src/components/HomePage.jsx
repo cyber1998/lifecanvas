4920bcf4c6137817cd5fe17c9201ebd2a4c18ae9
Cyber <cybr1998@gmail.com>
2023-06-24 17:38:52 +0530

    journal as a dropdown

--- canvas_fe/src/components/HomePage.jsx
+++ canvas_fe/src/components/HomePage.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, Nav, NavItem, NavLink } from 'reactstrap';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Profile from './users/Profile';
-import Home from './Home';
-import JournalPage from './journals/JournalPage';
-
-
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle
+} from "reactstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Profile from "./users/Profile";
+import Home from "./Home";
+import JournalPage from "./journals/JournalPage";
 
 const HomePage = (props) => {
   const [isOpen, setIsOpen] = useState(false);
   const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [journalDropdownOpen, setJournalDropdownOpen] = useState(false);
 
   const toggleProfile = () => {
     setIsProfileOpen((prevIsProfileOpen) => !prevIsProfileOpen);
@@ -20,17 +31,17 @@ const HomePage = (props) => {
   };
 
   const logOut = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('refresh');
-    window.location.href = '/';
+    localStorage.removeItem("token");
+    localStorage.removeItem("refresh");
+    window.location.href = "/";
   };
 
   // Decode jwt to get the user from the request
   const getUser = () => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
-      const base64Url = token.split('.')[1];
-      const base64 = base64Url.replace('-', '+').replace('_', '/');
+      const base64Url = token.split(".")[1];
+      const base64 = base64Url.replace("-", "+").replace("_", "/");
       const user = JSON.parse(window.atob(base64));
       return user;
     }
@@ -43,6 +54,9 @@ const HomePage = (props) => {
     setIsOpen((prevIsOpen) => !prevIsOpen);
   };
 
+  const journalDropdownToggle = () => setJournalDropdownOpen(!journalDropdownOpen);
+
+
   return (
     <>
       <div className="container">
@@ -52,11 +66,20 @@ const HomePage = (props) => {
           <Collapse isOpen={isOpen} navbar>
             <Nav className="me-auto" navbar>
               <NavItem>
-                <NavLink href="#" onClick={() => openProfileModal()}>Profile</NavLink>
+                <NavLink href="#" onClick={() => openProfileModal()}>
+                  Profile
+                </NavLink>
               </NavItem>
-              <NavItem>
-                <NavLink href="/journals">My Journals</NavLink>
-              </NavItem>
+              <Dropdown nav isOpen={journalDropdownOpen} toggle={journalDropdownToggle}>
+                <DropdownToggle nav caret>
+                  Journals
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem href="/journals">My Journals</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Journal Management</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
               <NavItem>
                 <NavLink href="/" onClick={logOut}>
                   Logout
@@ -68,14 +91,17 @@ const HomePage = (props) => {
       </div>
       <Router>
         <Routes>
-          <Route path="/" element={<Home user={user}/>} />
+          <Route path="/" element={<Home user={user} />} />
           <Route path="/journals" element={<JournalPage />} />
         </Routes>
       </Router>
-      {isProfileOpen && 
-      <Profile userId={user.user_id}
-      toggle={toggleProfile}
-      isOpen={isProfileOpen} />}
+      {isProfileOpen && (
+        <Profile
+          userId={user.user_id}
+          toggle={toggleProfile}
+          isOpen={isProfileOpen}
+        />
+      )}
     </>
   );
 };
